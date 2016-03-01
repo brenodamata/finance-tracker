aaf8b36010ff7f55df015d10e95f9cfefd6d6f4d
brenodamata <bjrdamata@gmail.com>
2016-03-01 15:40:44 -0500

    Ajax for search friends

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :user_stocks, except: [:show, :edit, :update]
 
+  resources :users, only: :show
+  resources :friendships
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -14,6 +17,10 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search_stocks"
 
   get 'my_friends', to: "users#my_friends"
+
+  get 'search_friends', to: 'users#search'
+
+  post 'add_friend', to: 'users#add_friend'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
